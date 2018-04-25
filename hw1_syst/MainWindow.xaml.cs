f22fbcd881f986236723e59f525b8e2e680c556d
ТогузбаевА <abai2010@bk.ru>
2018-04-25 11:01:50 +0600

    1Commit

---
+++ hw1_syst/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace hw1_syst
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProccesses_Click(object sender, RoutedEventArgs e)
+        {
+            Process[] processes = Process.GetProcesses();
+
+            var result = from p in processes
+                         select new
+                         {
+                             Id = p.Id,
+                             Name = p.ProcessName
+                             //State = p.
+                             //UserName = p.
+                         };
+
+            myDataGrid.ItemsSource = result;
+        }
+    }
+}
